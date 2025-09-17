2f7469b4049c3cbfb144d1bf1f5fa16123489b53
SadaieNaoya <c241020@ivy.ac.jp>
2025-09-17 14:21:58 +0900

    プロジェクト追加 現状は初期段階で検索機能の実装を行っている。 GASから検索結果を受け取るとこまで成功

---
+++ snippets/App.vue
@@ -0,0 +1,41 @@
+  <template>
+    <div id="app">
+      <router-view :key="$route.fullPath"></router-view>
+      <loading></loading>
+    </div>
+  </template>
+
+<script>
+import loading from '@/components/CompLoading'
+export default {
+  name: 'app',
+  components: {
+    loading,
+  },
+  mounted() { },
+  methods: {
+    windowResize: function () {
+      this.windowWidth = window.innerWidth;
+      this.windowHeight = window.innerHeight - 100;
+      this.setWindowHeight();
+    },
+    setWindowHeight() {
+      document.documentElement.style.setProperty('--window-height', this.windowHeight);
+    }
+  },
+  mounted: function () {
+    window.addEventListener('resize', this.windowResize)
+    this.setWindowHeight();
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Hiragino Kaku Gothic Pro', 'ヒラギノ角ゴ Pro W3', Meiryo, メイリオ, Osaka, 'MS PGothic', arial, helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
